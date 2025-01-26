be1e20bb5b845442eaaf86154288e1b30d9da832
KingKayden <108900206+KingKayden1@users.noreply.github.com>
2025-01-26 16:01:20 -0600

    Added a standalone option for the notification

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.Notify = 'Set_ME'  ---------- options for now are qb and ox
+Config.Notify = 'Set_ME'  ---------- options for now are qb and ox and standalone
 
 
 ---------------------------------- Made By StarFallTeam  (Kayden)
